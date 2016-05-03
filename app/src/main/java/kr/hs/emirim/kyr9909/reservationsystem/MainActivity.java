998a84cfc8c8193ed3771fba724eb1b7b052eccb
Yurim Kang <kyr9909@naver.com>
2016-05-03 20:17:54 +0900

    new project

---
+++ app/src/main/java/kr/hs/emirim/kyr9909/reservationsystem/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.kyr9909.reservationsystem;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
