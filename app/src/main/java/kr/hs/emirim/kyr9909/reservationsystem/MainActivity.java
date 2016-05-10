2e8ef751f5e7783540fc34114195a3df5d8cf7c3
Yurim Kang <kyr9909@naver.com>
2016-05-10 20:58:24 +0900

    #4 Third Event Programming

--- app/src/main/java/kr/hs/emirim/kyr9909/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/reservationsystem/MainActivity.java
@@ -1,5 +1,7 @@
 package kr.hs.emirim.kyr9909.reservationsystem;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.widget.Chronometer;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -53,6 +57,32 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
                 calView.setVisibility(View.INVISIBLE);
                 timePick.setVisibility(View.VISIBLE);
+            }
+        });
+
+        butStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+            }
+        });
+
+        butDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+                Calendar cal=Calendar.getInstance();
+                cal.setTimeInMillis(calView.getDate()); //캘린더 객체를 만들기 위해서
+                int year=cal.get(Calendar.YEAR);
+                int month=cal.get(Calendar.MONTH)+1;
+                int date=cal.get(Calendar.DATE);
+                //int hour=timePick.getHour();
+                //int minute=timePick.getMinute();
+                String dateAndTime=year+"년 "+month+"월 "+date+"일 ";
+                textResult.setText(dateAndTime);
             }
         });
     }
